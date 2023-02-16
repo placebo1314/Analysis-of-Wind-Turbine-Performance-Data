169e70b5f70c8d8d2323ed272825d5b9a6d2ae9d
Krisz <placebo1314@gmail.com>
2023-02-16 15:29:37 +0100

    Add project files.

---
+++ Main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+#pip install scikit-learn
+
+data = pd.read_csv('wind_turbine_data.csv')
